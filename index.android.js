d4ee4dcd9505574a16cbd24823ca1cc41002aba7
Vinay Venu <ijkvinay@gmail.com>
2017-06-21 11:43:43 +0530

    Add lines in chart

--- index.android.js
+++ index.android.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import {
-    AppRegistry, ScrollView, Text, TextInput
-} from 'react-native';
-import {VictoryChart} from 'victory-native';
+import {AppRegistry, ScrollView, Text, TextInput, View} from 'react-native';
+import {VictoryChart, VictoryLine} from 'victory-native';
 
 
 export default class victoryDemo extends Component {
 
     componentWillMount() {
-        this.setUpGraphs('5');
+        this.setUpCharts('5', '1');
     }
 
     componentDidMount() {
@@ -19,30 +17,48 @@ export default class victoryDemo extends Component {
         this.changeTimeToMountText();
     }
 
-    setUpGraphs(numberOfGraphsStr) {
+    setUpCharts(numberOfChartsStr, numberOfLinesStr) {
         this.setState({
             renderStartTime: new Date(),
-            numberOfGraphs: numberOfGraphsStr,
+            numberOfCharts: numberOfChartsStr,
+            numberOfLines: numberOfLinesStr,
             shouldChangeText: true
         });
-    }
-
-    numberOfGraphs() {
-        const isNumeric = (str) => !isNaN(parseFloat(str)) && isFinite(str);
-        return isNumeric(this.state.numberOfGraphs) ? parseInt(this.state.numberOfGraphs) : 0;
     }
 
     changeTimeToMountText() {
         if (!this.state.shouldChangeText) return;
 
-        const timeToMountText = "Mounting of " + this.state.numberOfGraphs + " empty graphs took " + (new Date() - this.state.renderStartTime) + " ms";
-        this.setState({shouldChangeText: false, timeToMountText})
+        const timeToMountText = "Mounting of " + this.state.numberOfCharts + " empty Charts took " + (new Date() - this.state.renderStartTime) + " ms";
+        this.setState({shouldChangeText: false, timeToMountText: timeToMountText})
     }
 
-    renderGraphs() {
+    toNumber(valueStr) {
+        const isNumeric = (str) => !isNaN(parseFloat(str)) && isFinite(str);
+        return isNumeric(valueStr) ? parseInt(valueStr) : 0;
+    }
+
+    renderLines() {
+        const lines = [],
+            xAxis = Array.from({length: 30}, (v, i) => i),
+            data = () => {
+                return xAxis.map((item) => {
+                    return {x: item, y: Math.floor((Math.random() * 10) + 1)}
+                });
+            };
+
+        for (let index = 0; index < this.toNumber(this.state.numberOfLines); index++) {
+            lines.push(<VictoryLine data={data()} key={index}/>)
+        }
+        return lines;
+    }
+
+    renderCharts() {
         const charts = [];
-        for (var index = 0; index < this.numberOfGraphs(); index++) {
-            charts.push(<VictoryChart key={index}/>);
+        for (let index = 0; index < this.toNumber(this.state.numberOfCharts); index++) {
+            charts.push(<VictoryChart key={index}>
+                {this.renderLines()}
+            </VictoryChart>);
         }
         return charts;
     }
@@ -50,10 +66,19 @@ export default class victoryDemo extends Component {
     render() {
         return (
             <ScrollView>
-                <Text style={{fontSize: 24}}>{this.state.changeTimeToMountText}</Text>
-                <TextInput value={this.state.numberOfGraphs}
-                           onChangeText={(numStr) => this.setUpGraphs(numStr)}></TextInput>
-                {this.renderGraphs()}
+                <Text style={{fontSize: 24}}>{this.state.timeToMountText}</Text>
+                <View style={{flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'flex-start'}}>
+                    <Text style={{flex: 1}}>Number of charts</Text>
+                    <TextInput style={{flex: 1}} value={this.state.numberOfCharts}
+                               onChangeText={(numberOfChartsStr) => this.setUpCharts(numberOfChartsStr, this.state.numberOfLines)}/>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
+                    <Text style={{flex: 1}}>Number of lines in chart</Text>
+                    <TextInput  style={{flex: 1}} value={this.state.numberOfLines}
+                               onChangeText={(numberOfLinesStr) => this.setUpCharts(this.state.numberOfCharts, numberOfLinesStr)}/>
+                </View>
+
+                {this.renderCharts()}
             </ScrollView>
         );
     }
